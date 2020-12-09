5c70ed6f8927355e3e34f5b08908b478c9ae9cb4
Paolo <23174862+codePaolo@users.noreply.github.com>
2020-12-09 12:17:31 -0800

    Initial Commit
    Initial Commit

---
+++ pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>About Dad Jokes</h1>
+    <p>This is an app that grabs data from an API.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "About The App",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best place for corny dad jokes"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
